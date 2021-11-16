fe916da85d19fca5d0a85d6c761baca7f22a505b
rodriPerez19 <ropeque19@hotmail.com>
2021-11-16 17:02:18 -0300

    [update body]

--- app.js
+++ app.js
@@ -17,13 +17,15 @@ let users = ["bart", "lisa", "homero", "marge"];
 
 app.use(cors());
 app.use(methodOverride());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/users", (req, res)=>{
    res.send(users);
 });
 
-app.post("/user/create/:nombre", (req, res)=>{
-   users.push(req.params.nombre);
+app.post("/user/create/", (req, res)=>{
+   users.push(req.body.nombre);
    res.send("usuario creado");
 });
 
